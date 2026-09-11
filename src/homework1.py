991cfc7723d5d587fb1bdf71f0d70bd78dc52c5f
Kiril Chervoniak <42374203+kirilche@users.noreply.github.com>
2026-09-11 11:57:59 +0000

    add Homework 1

---
+++ src/homework1.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+# Replace with correct types
+
+
+a: Any = 100
+b: Any = 10.3
+c: Any = False
+d: Any = None
+e: Any = "Jack"
+f: Any = (10, 20)
+g: Any = [14, 25, 37]
+h: Any = ["Me", "My Family", "My Friends"]
+i: Any = [(12.4, 3), (15.6, 7), (30.2, 1)]
